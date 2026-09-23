fb4159bf8f408b681f9b924d6408644e0485ef5c
B1ngB0ngLing <183355883+B1ngB0ngL1ng@users.noreply.github.com>
2026-09-24 00:23:42 +0200

    verdere setup gedaan van de test files zodat het er hetzelfde uitziet als de warehouses maar dan met de juiste naam

--- tests/test_endpoint_item_types.py
+++ tests/test_endpoint_item_types.py
@@ -1,10 +1,25 @@
 import pytest
 import requests
 
-
+# zorg dat je zelf de juiste api key invult voor de werkende tests 
+# en mocht je een foute key willen hebben qua test let er dan op. 
 @pytest.fixture
 def _data():
     return {
         'url': 'http://localhost:3000/api/v1/',
         'api_key': 'a1b2c3d4e5',
     }
+
+
+
+def test_get_item_type(_data):
+    url = _data['url'] + 'item_type'
+
+    # Send a GET request to the API
+    response = requests.get(url, headers={'API_KEY': _data['api_key']})
+
+    # Get the status code and response data
+    status_code = response.status_code
+
+    # Verify that the status code is 200 (OK)
+    assert status_code == 200
